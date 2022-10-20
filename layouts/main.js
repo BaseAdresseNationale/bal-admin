e07f99a5f0614819725e620205e9af0f6de59be8
Guillaume <guillaume@livingdata.co>
2022-10-20 14:19:25 +0200

    Remove white background

--- layouts/main.js
+++ layouts/main.js
@@ -35,7 +35,6 @@ class Layout extends React.Component {
         <style jsx>{`
           main {
             flex: 1;
-            background-color: #fff;
           }
         `}
         </style>
