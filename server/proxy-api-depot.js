11befb2dacab0210342038f14f8bb0a0144547d7
Théophile Merlière <theophile.merliere@gmail.com>
2023-01-04 14:31:42 +0100

    Add api-depot method for clients, mandataires and chefDeFile

--- server/proxy-api-depot.js
+++ server/proxy-api-depot.js
@@ -19,13 +19,38 @@ function forward(gotResponse, res) {
   res.status(gotResponse.statusCode).send(gotResponse.body)
 }
 
+async function createClient(req, res) {
+  const response = await client.post('clients', {json: req.body})
+  forward(response, res)
+}
+
 async function getClients(req, res) {
   const response = await client.get('clients')
   forward(response, res)
 }
 
+async function getClient(req, res) {
+  const response = await client.get(`clients/${req.params.clientId}`)
+  forward(response, res)
+}
+
+async function updateClient(req, res) {
+  const response = await client.put(`clients/${req.params.clientId}`, {json: req.body})
+  forward(response, res)
+}
+
+async function getMandataire(req, res) {
+  const response = await client.get(`mandataires/${req.params.mandataireId}`)
+  forward(response, res)
+}
+
 async function getMandataires(req, res) {
   const response = await client.get('mandataires')
+  forward(response, res)
+}
+
+async function getChefDeFile(req, res) {
+  const response = await client.get(`chefs-de-file/${req.params.chefDeFileId}`)
   forward(response, res)
 }
 
@@ -39,13 +64,18 @@ const app = new express.Router()
 app.use(express.json())
 
 // Clients
+app.post('/clients', w(createClient))
 app.get('/clients', w(getClients))
+app.get('/clients/:clientId', w(getClient))
+app.put('/clients/:clientId', w(updateClient))
 
 // Mandataires
 app.get('/mandataires', w(getMandataires))
+app.get('/mandataires/:mandataireId', w(getMandataire))
 
 // Chefs de file
 app.get('/chefs-de-file', w(getChefsDeFile))
+app.get('/chefs-de-file/:chefDeFileId', w(getChefDeFile))
 
 module.exports = app
 
