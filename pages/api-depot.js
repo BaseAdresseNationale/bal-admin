d1fc2031a490c87d56000e7e8d20e3877a7e41b3
Théophile Merlière <theophile.merliere@gmail.com>
2023-03-30 14:14:28 +0200

    Center api-depot tabs

--- pages/api-depot.js
+++ pages/api-depot.js
@@ -16,51 +16,52 @@ const APIDepot = () => {
       <Loader isLoading={isLoading}>
 
         {isAdmin && (
-          <Tabs
-            className='fr-container fr-my-2w'
-            tabs={[
-              {label: 'Production', iconId: 'fr-icon-plant-fill', content: (
-                <div className='fr-container fr-py-12v'>
+          <div className='fr-container'>
+            <Tabs
+              className='fr-container fr-my-2w'
+              tabs={[
+                {label: 'Production', iconId: 'fr-icon-plant-fill', content: (
+                  <div className='fr-container fr-py-12v'>
 
-                  <div className='fr-grid-row fr-grid-row--gutters fr-grid-row--right'>
-                    <div className='fr-col-2'>
-                      <Link passHref href={{
-                        pathname: '/api-depot/client/client-form',
-                      }}
-                      >
-                        <Button iconId='fr-icon-add-line'>
-                          Ajouter un client
-                        </Button>
-                      </Link>
+                    <div className='fr-grid-row fr-grid-row--gutters fr-grid-row--right'>
+                      <div className='fr-col-2'>
+                        <Link passHref href={{
+                          pathname: '/api-depot/client/client-form',
+                        }}
+                        >
+                          <Button iconId='fr-icon-add-line'>
+                            Ajouter un client
+                          </Button>
+                        </Link>
+                      </div>
                     </div>
+
+                    <ClientsList />
                   </div>
+                )},
+                {label: 'Démonstration', iconId: 'fr-icon-seedling-fill', content: (
 
-                  <ClientsList />
-                </div>
-              )},
-              {label: 'Démonstration', iconId: 'fr-icon-seedling-fill', content: (
+                  <div className='fr-container fr-py-12v'>
 
-                <div className='fr-container fr-py-12v'>
-
-                  <div className='fr-grid-row fr-grid-row--gutters fr-grid-row--right'>
-                    <div className='fr-col-2'>
-                      <Link passHref href={{
-                        pathname: '/api-depot/client/client-form?demo=1',
-                      }}
-                      >
-                        <Button iconId='fr-icon-add-line'>
-                          Ajouter un client
-                        </Button>
-                      </Link>
+                    <div className='fr-grid-row fr-grid-row--gutters fr-grid-row--right'>
+                      <div className='fr-col-2'>
+                        <Link passHref href={{
+                          pathname: '/api-depot/client/client-form?demo=1',
+                        }}
+                        >
+                          <Button iconId='fr-icon-add-line'>
+                            Ajouter un client
+                          </Button>
+                        </Link>
+                      </div>
                     </div>
+
+                    <ClientsList isDemo />
                   </div>
-
-                  <ClientsList isDemo />
-                </div>
-              )}
-            ]}
-          />
-
+                )}
+              ]}
+            />
+          </div>
         )}
       </Loader>
     </Main>
