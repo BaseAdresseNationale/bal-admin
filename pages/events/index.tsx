6e2e839078b22f293f6ac81a3a6b042b17718504
Guillaume Fay <guillaume_fay@hotmail.com>
2023-11-24 17:07:30 +0100

    wip

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {toast} from 'react-toastify'
+import { toast } from "react-toastify";
 import Link from "next/link";
 
 import { Button } from "@codegouvfr/react-dsfr/Button";
-import {Tabs} from '@codegouvfr/react-dsfr/Tabs'
-import {Badge} from '@codegouvfr/react-dsfr/Badge'
+import { Tabs } from "@codegouvfr/react-dsfr/Tabs";
+import { Badge } from "@codegouvfr/react-dsfr/Badge";
 
 import { EditableList } from "../../components/editable-list";
 import { getEvents, massImportEvents } from "../../lib/events";
@@ -27,108 +27,140 @@ const EventsPage = ({ incommingEvents, pastEvents }: EventsPageProps) => {
 
   const onMassImport = async () => {
     try {
-      await massImportEvents(massImportData)
-      toast("Les évènements ont bien été importés", {type: 'success'})
+      await massImportEvents(massImportData);
+      toast("Les évènements ont bien été importés", { type: "success" });
     } catch (error: unknown) {
-      toast("Un problème est survenu pendant l'import, aucun évènement n'a été importé", {type: 'error'})
+      toast(
+        "Un problème est survenu pendant l'import, aucun évènement n'a été importé",
+        { type: "error" }
+      );
       console.log(error);
     }
 
-  return (  <div className="fr-container">
-  <Tabs
-className='fr-container fr-my-2w'
-tabs={[
-  {
-    label: <><Badge style={{marginRight: 10}} noIcon>{incommingEvents.length}</Badge> {`Évènement${incommingEvents.length > 1 ? 's' : ''} à venir`}</>,
-    content: (
-      incommingEvents.length === 0 ? 'Aucun évènement à venir' : <EditableList
-      headers={["Type", "Nom", "Date", "Horaires", '']}
-      caption="Liste des évènements"
-      data={incommingEvents}
-      filter={{
-        placeholder: "Filtrer par nom",
-        property: "name",
-      }}
-      createBtn={
-        <div className="fr-grid-row fr-grid-row--gutters fr-grid-row--right">
-          <div className="fr-col-2">
-            <Button onClick={() => massImportEventsModale.open()} iconId="fr-icon-add-line">Import en masse</Button>
-          </div>
-          <div className="fr-col-2">
-            <Link
-              passHref
-              href={{
-                pathname: "/events/new",
+    return (
+      <div className="fr-container">
+        <Tabs
+          className="fr-container fr-my-2w"
+          tabs={[
+            {
+              label: (
+                <>
+                  <Badge style={{ marginRight: 10 }} noIcon>
+                    {incommingEvents.length}
+                  </Badge>{" "}
+                  {`Évènement${incommingEvents.length > 1 ? "s" : ""} à venir`}
+                </>
+              ),
+              content:
+                incommingEvents.length === 0 ? (
+                  "Aucun évènement à venir"
+                ) : (
+                  <EditableList
+                    headers={["Type", "Nom", "Date", "Horaires", ""]}
+                    caption="Liste des évènements"
+                    data={incommingEvents}
+                    filter={{
+                      placeholder: "Filtrer par nom",
+                      property: "name",
+                    }}
+                    createBtn={
+                      <div className="fr-grid-row fr-grid-row--gutters fr-grid-row--right">
+                        <div className="fr-col-2">
+                          <Button
+                            onClick={() => massImportEventsModale.open()}
+                            iconId="fr-icon-add-line"
+                          >
+                            Import en masse
+                          </Button>
+                        </div>
+                        <div className="fr-col-2">
+                          <Link
+                            passHref
+                            href={{
+                              pathname: "/events/new",
+                            }}
+                          >
+                            <Button iconId="fr-icon-add-line">
+                              Créer un évènement
+                            </Button>
+                          </Link>
+                        </div>
+                      </div>
+                    }
+                    renderItem={EventItem}
+                  />
+                ),
+            },
+            {
+              label: (
+                <>
+                  <Badge style={{ marginRight: 10 }} noIcon>
+                    {pastEvents.length}
+                  </Badge>{" "}
+                  {`Évènement${pastEvents.length > 1 ? "s" : ""} passés`}
+                </>
+              ),
+              content: (
+                <EditableList
+                  headers={["Type", "Nom", "Date", "Horaires", ""]}
+                  caption="Liste des évènements"
+                  data={pastEvents}
+                  filter={{
+                    placeholder: "Filtrer par nom",
+                    property: "name",
+                  }}
+                  renderItem={EventItem}
+                />
+              ),
+            },
+          ]}
+        />
+        <massImportEventsModale.Component title="Import en masse">
+          <p>
+            Vous pouvez importer des évènements en masse en collant le contenu
+            d&apos;un fichier JSON dans le champ ci-dessous.
+          </p>
+          <textarea
+            style={{ width: "100%", height: "10rem" }}
+            placeholder="Coller le contenu du fichier JSON"
+            value={massImportData}
+            onChange={(e) => setMassImportData(e.target.value)}
+          />
+          <div>
+            <Button onClick={onMassImport}>Importer</Button>
+            <Button
+              style={{ marginLeft: "1rem" }}
+              priority="tertiary"
+              onClick={() => {
+                massImportEventsModale.close();
               }}
             >
-              <Button iconId="fr-icon-add-line">Créer un évènement</Button>
-            </Link>
+              Annuler
+            </Button>
           </div>
-        </div>
-      }
-      renderItem={EventItem}
-    />
-    ),
-  },
-  {
-    label: <><Badge style={{marginRight: 10}} noIcon>{pastEvents.length}</Badge> {`Évènement${pastEvents.length > 1 ? 's' : ''} passés`}</>,
-    content: (
-      <EditableList
-      headers={["Type", "Nom", "Date", "Horaires", '']}
-      caption="Liste des évènements"
-      data={pastEvents}
-      filter={{
-        placeholder: "Filtrer par nom",
-        property: "name",
-      }}
-      renderItem={EventItem}
-    />
-    ),
-  },
-]}
-/>
-<massImportEventsModale.Component title="Import en masse">
-        <p>
-          Vous pouvez importer des évènements en masse en collant le contenu d'un
-          fichier JSON dans le champ ci-dessous.
-        </p>
-        <textarea
-          style={{ width: "100%", height: "10rem" }}
-          placeholder="Coller le contenu du fichier JSON"
-          value={massImportData}
-          onChange={(e) => setMassImportData(e.target.value)}
-        />
-        <div>
-          <Button onClick={onMassImport}>Importer</Button>
-          <Button
-            style={{ marginLeft: "1rem" }}
-            priority="tertiary"
-            onClick={() => {
-              massImportEventsModale.close();
-            }}
-          >
-            Annuler
-          </Button>
-        </div>
-      </massImportEventsModale.Component>
-</div>)
-}
+        </massImportEventsModale.Component>
+      </div>
+    );
+  };
+};
 
 export async function getServerSideProps() {
   const events = await getEvents();
 
-  const incommingEvents = events.filter((event) => {
-    return new Date(event.date) > new Date();
-  }) || []
+  const incommingEvents =
+    events.filter((event) => {
+      return new Date(event.date) > new Date();
+    }) || [];
 
-  const pastEvents = events.filter((event) => {
-    return new Date(event.date) < new Date();
-  }) || []
+  const pastEvents =
+    events.filter((event) => {
+      return new Date(event.date) < new Date();
+    }) || [];
 
   return {
     props: {
       incommingEvents,
-      pastEvents
+      pastEvents,
     },
   };
 }
