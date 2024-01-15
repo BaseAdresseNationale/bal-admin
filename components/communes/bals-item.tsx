83e7e76865c983f583ea76bb41f65da16d3c58c3
Fufeck <fabien.tafforeau@gmail.com>
2024-01-15 12:31:09 +0100

    fix waiting habilitation

--- components/communes/bals-item.tsx
+++ components/communes/bals-item.tsx
@@ -1,41 +1,69 @@
-import Link from 'next/link'
-import {Badge} from '@codegouvfr/react-dsfr/Badge'
-import {Button} from '@codegouvfr/react-dsfr/Button'
+import Link from "next/link";
+import { useEffect, useMemo, useState } from "react";
+import { Badge } from "@codegouvfr/react-dsfr/Badge";
+import { Button } from "@codegouvfr/react-dsfr/Button";
 
-import type {BaseLocaleType} from '../../types/mes-adresses'
-import {computeStatus} from '@/lib/bal-status'
-import {formatDate} from '@/lib/util/date'
+import type { BaseLocaleType } from "../../types/mes-adresses";
+import { StatusInterface, computeStatus } from "@/lib/bal-status";
+import { getBaseLocaleIsHabilitationValid } from "@/lib/api-mes-adresses";
+import { formatDate } from "@/lib/util/date";
+import { AlertProps } from "@codegouvfr/react-dsfr/Alert";
 
-export const BalsItem = (item: BaseLocaleType, actions: Record<string, (item: BaseLocaleType) => void>) => {
-  const {_id, nom, status, sync, _created, _updated} = item
+export const BalsItem = (
+  item: BaseLocaleType,
+  actions: Record<string, (item: BaseLocaleType) => void>
+) => {
+  const { _id, nom, status, sync, _created, _updated } = item;
+  const [computedStatus, setComputedStatus] = useState<StatusInterface | null>(
+    null
+  );
+
+  useEffect(() => {
+    async function initStatus() {
+      const habilitationIsValid = await getBaseLocaleIsHabilitationValid(_id);
+      setComputedStatus(computeStatus(status, sync, habilitationIsValid));
+    }
+
+    void initStatus();
+  }, [_id, status, sync]);
+
   return (
     <tr key={_id}>
-      <td className='fr-col fr-my-1v'>{_id}</td>
-      <td className='fr-col fr-my-1v'>{nom}</td>
-      <td className='fr-col fr-my-1v'>
-        <Badge severity={computeStatus(status, sync).intent} noIcon>{computeStatus(status, sync).label}</Badge>
+      <td className="fr-col fr-my-1v">{_id}</td>
+      <td className="fr-col fr-my-1v">{nom}</td>
+      <td className="fr-col fr-my-1v">
+        <Badge severity={computedStatus?.intent} noIcon>
+          {computedStatus?.label}
+        </Badge>
       </td>
-      <td className='fr-col fr-my-1v'>
-        {_created ? formatDate(_created) : 'inconnu'}
+      <td className="fr-col fr-my-1v">
+        {_created ? formatDate(_created) : "inconnu"}
       </td>
-      <td className='fr-col fr-my-1v'>
-        {_updated ? formatDate(_updated) : 'inconnu'}
+      <td className="fr-col fr-my-1v">
+        {_updated ? formatDate(_updated) : "inconnu"}
       </td>
-      <td className='fr-col fr-my-1v'>
-        <Link passHref href={{pathname: '/mes-adresses/base-locale', query: {baseLocaleId: _id}}}>
-          <Button iconId='fr-icon-arrow-right-line' iconPosition='right'>
+      <td className="fr-col fr-my-1v">
+        <Link
+          passHref
+          href={{
+            pathname: "/mes-adresses/base-locale",
+            query: { baseLocaleId: _id },
+          }}
+        >
+          <Button iconId="fr-icon-arrow-right-line" iconPosition="right">
             Consulter
           </Button>
         </Link>
       </td>
-      <td className='fr-col fr-my-1v'>
-        <Button onClick={() => {
-          actions.delete(item)
-        }}
+      <td className="fr-col fr-my-1v">
+        <Button
+          onClick={() => {
+            actions.delete(item);
+          }}
         >
           Supprimer
         </Button>
       </td>
     </tr>
-  )
-}
+  );
+};
