fb0c4e1b2205a5937396b6efe115044ef12cdb52
Guillaume Fay <guillaume_fay@hotmail.com>
2024-04-12 11:19:18 +0200

    fix: new chef de file not selected on save

--- components/api-depot/client/client-form/chef-de-file-form.tsx
+++ components/api-depot/client/client-form/chef-de-file-form.tsx
@@ -1,89 +1,111 @@
-import {useEffect, useState} from 'react'
-import Button from '@codegouvfr/react-dsfr/Button'
-import Input from '@codegouvfr/react-dsfr/Input'
-import ToggleSwitch from '@codegouvfr/react-dsfr/ToggleSwitch'
+import { useEffect, useState } from "react";
+import Button from "@codegouvfr/react-dsfr/Button";
+import Input from "@codegouvfr/react-dsfr/Input";
+import ToggleSwitch from "@codegouvfr/react-dsfr/ToggleSwitch";
 
-import {checkEmail} from '@/lib/util/email'
+import { checkEmail } from "@/lib/util/email";
 
-import { ChefDeFileApiDepotType, PerimeterType } from 'types/api-depot'
-import { createChefDeFile, updateChefDeFile } from '@/lib/api-depot'
-import PerimeterList from '@/components/api-depot/client/client-form/perimeter-list'
+import { ChefDeFileApiDepotType, PerimeterType } from "types/api-depot";
+import { createChefDeFile, updateChefDeFile } from "@/lib/api-depot";
+import PerimeterList from "@/components/api-depot/client/client-form/perimeter-list";
 
 interface ChefDeFileFormProps {
   initialChefDeFile: ChefDeFileApiDepotType;
   isDemo: boolean;
   close: () => void;
+  onSelect: (value: string) => void;
 }
 
-const ChefDeFileForm = ({initialChefDeFile, isDemo, close}: ChefDeFileFormProps) => {
-  const [nom, setNom] = useState<string>(initialChefDeFile ? initialChefDeFile.nom : '')
-  const [email, setEmail] = useState<string>(initialChefDeFile ? initialChefDeFile.email : '')
-  const [isEmailPublic, setIsEmailPublic] = useState<boolean>(initialChefDeFile ? initialChefDeFile.isEmailPublic : true)
-  const [isEmailValid, setIsEmailValid] = useState<boolean>(true)
-  const [perimeters, setPerimeters] = useState<PerimeterType[]>(initialChefDeFile ? initialChefDeFile.perimetre : [])
-  const [isSignataireCharte, setIsSignataireCharte] = useState<boolean>(initialChefDeFile ? initialChefDeFile.signataireCharte : false)
+const ChefDeFileForm = ({
+  initialChefDeFile,
+  isDemo,
+  close,
+  onSelect,
+}: ChefDeFileFormProps) => {
+  const [nom, setNom] = useState<string>(
+    initialChefDeFile ? initialChefDeFile.nom : ""
+  );
+  const [email, setEmail] = useState<string>(
+    initialChefDeFile ? initialChefDeFile.email : ""
+  );
+  const [isEmailPublic, setIsEmailPublic] = useState<boolean>(
+    initialChefDeFile ? initialChefDeFile.isEmailPublic : true
+  );
+  const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
+  const [perimeters, setPerimeters] = useState<PerimeterType[]>(
+    initialChefDeFile ? initialChefDeFile.perimetre : []
+  );
+  const [isSignataireCharte, setIsSignataireCharte] = useState<boolean>(
+    initialChefDeFile ? initialChefDeFile.signataireCharte : false
+  );
 
-  const saveChefDeFile = async event => {
-    event.preventDefault()
+  const saveChefDeFile = async (event) => {
+    event.stopPropagation();
+    event.preventDefault();
     try {
       const chefDeFile = {
         nom,
         email,
         isEmailPublic,
         perimetre: perimeters,
-        signataireCharte: isSignataireCharte
-      }
+        signataireCharte: isSignataireCharte,
+      };
       if (initialChefDeFile?._id) {
-        await updateChefDeFile(initialChefDeFile._id, chefDeFile, isDemo)
+        await updateChefDeFile(initialChefDeFile._id, chefDeFile, isDemo);
       } else {
-        await createChefDeFile(chefDeFile, isDemo)
+        const newChefDeFile = await createChefDeFile(chefDeFile, isDemo);
+        onSelect(newChefDeFile._id);
       }
-      close()
+      close();
     } catch (error: unknown) {
-      console.error(error)
+      console.error(error);
     }
-  }
-
-  const closePreventDefault = event => {
-    event.preventDefault();
-    close();
-  }
-
-  const handleEditNom = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { value } = e.target;
-    setNom(value)
   };
 
-  const handleEditEmail = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const closePreventDefault = (event) => {
+    event.preventDefault();
+    close();
+  };
+
+  const handleEditNom = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { value } = e.target;
-    setEmail(value)
+    setNom(value);
+  };
+
+  const handleEditEmail = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { value } = e.target;
+    setEmail(value);
   };
 
   useEffect(() => {
     if (email) {
-      const isValid = checkEmail(email)
-      setIsEmailValid(isValid)
+      const isValid = checkEmail(email);
+      setIsEmailValid(isValid);
     } else {
-      setIsEmailValid(null)
+      setIsEmailValid(null);
     }
-  }, [email])
+  }, [email]);
 
   return (
-    <div className='fr-my-4w'>
-      <div onSubmit={saveChefDeFile}>
-        <label className='fr-label'>Chef de file</label>
+    <div className="fr-my-4w">
+      <form onSubmit={saveChefDeFile}>
+        <label className="fr-label">Chef de file</label>
 
         <ToggleSwitch
-          label='Signataire de la charte'
+          label="Signataire de la charte"
           checked={isSignataireCharte}
-          helperText='Le chef de file a signé la charte'
+          helperText="Le chef de file a signé la charte"
           onChange={setIsSignataireCharte}
         />
 
-        <div className='fr-grid-row fr-grid-row--gutters'>
-          <div className='fr-col-4'>
+        <div className="fr-grid-row fr-grid-row--gutters">
+          <div className="fr-col-4">
             <Input
-              label='Nom'
+              label="Nom"
               nativeInputProps={{
                 required: true,
                 value: nom,
@@ -91,40 +113,43 @@ const ChefDeFileForm = ({initialChefDeFile, isDemo, close}: ChefDeFileFormProps)
               }}
             />
           </div>
-          <div className='fr-col-4'>
+          <div className="fr-col-4">
             <Input
-              label='Email'
+              label="Email"
               nativeInputProps={{
                 required: true,
                 value: email,
                 onChange: handleEditEmail,
               }}
-              state={isEmailValid === false ? 'error' : 'default'}
-              stateRelatedMessage='L’email n’est pas valide'
+              state={isEmailValid === false ? "error" : "default"}
+              stateRelatedMessage="L’email n’est pas valide"
             />
           </div>
-          <div className='fr-col-4'>
-              <ToggleSwitch
-                label='Email public'
-                checked={isEmailPublic}
-                onChange={setIsEmailPublic}
-              />
-            </div>
+          <div className="fr-col-4">
+            <ToggleSwitch
+              label="Email public"
+              checked={isEmailPublic}
+              onChange={setIsEmailPublic}
+            />
+          </div>
         </div>
 
-        <PerimeterList perimeters={perimeters} handlePerimeter={setPerimeters} />
+        <PerimeterList
+          perimeters={perimeters}
+          handlePerimeter={setPerimeters}
+        />
 
-        <div className='fr-my-4w fr-grid-row'>
-          <Button priority='primary' onClick={e => saveChefDeFile(e)}>
+        <div className="fr-my-4w fr-grid-row">
+          <Button priority="primary" onClick={saveChefDeFile}>
             Enregistrer
           </Button>
-          <Button priority='secondary' onClick={e => closePreventDefault(e)}>
+          <Button priority="secondary" onClick={closePreventDefault}>
             Annuler
           </Button>
         </div>
-      </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ChefDeFileForm
+export default ChefDeFileForm;
--- pages/api-depot/client/client-form.tsx
+++ pages/api-depot/client/client-form.tsx
@@ -1,57 +1,74 @@
-import {useCallback, useEffect, useState} from 'react'
-import {Button} from '@codegouvfr/react-dsfr/Button'
-import {Alert} from '@codegouvfr/react-dsfr/Alert'
-import {ToggleSwitch} from '@codegouvfr/react-dsfr/ToggleSwitch'
-import {useRouter} from 'next/router'
+import { useCallback, useEffect, useState } from "react";
+import { Button } from "@codegouvfr/react-dsfr/Button";
+import { Alert } from "@codegouvfr/react-dsfr/Alert";
+import { ToggleSwitch } from "@codegouvfr/react-dsfr/ToggleSwitch";
+import { useRouter } from "next/router";
 
-import type {ChefDeFileApiDepotType, MandataireApiDepotType} from 'types/api-depot'
+import type {
+  ChefDeFileApiDepotType,
+  MandataireApiDepotType,
+} from "types/api-depot";
 
-import {createClient, updateClient, createMandataire, createChefDeFile, getChefsDeFile, getClient, getMandataires} from '@/lib/api-depot'
-import Loader from '@/components/loader'
-import SelectInput from '@/components/select-input'
-import TextInput from '@/components/text-input'
-import MandataireForm from '@/components/api-depot/client/client-form/mandataire-form'
-import ChefDeFileSelect from '@/components/api-depot/client/client-form/chef-de-file-select'
-import { object } from 'prop-types'
-import ChefDeFileForm from '@/components/api-depot/client/client-form/chef-de-file-form'
+import {
+  createClient,
+  updateClient,
+  createMandataire,
+  createChefDeFile,
+  getChefsDeFile,
+  getClient,
+  getMandataires,
+} from "@/lib/api-depot";
+import Loader from "@/components/loader";
+import SelectInput from "@/components/select-input";
+import TextInput from "@/components/text-input";
+import MandataireForm from "@/components/api-depot/client/client-form/mandataire-form";
+import ChefDeFileSelect from "@/components/api-depot/client/client-form/chef-de-file-select";
+import { object } from "prop-types";
+import ChefDeFileForm from "@/components/api-depot/client/client-form/chef-de-file-form";
 
 const authorizationStrategyOptions = [
-  {label: 'Chef de file', value: 'chef-de-file'},
-  {label: 'Habilitation', value: 'habilitation'},
-  {label: 'Interne', value: 'internal'},
-]
+  { label: "Chef de file", value: "chef-de-file" },
+  { label: "Habilitation", value: "habilitation" },
+  { label: "Interne", value: "internal" },
+];
 
 const newClientFormData = {
-  nom: '',
+  nom: "",
   isModeRelax: false,
   isActive: false,
-  authorizationStrategy: 'chef-de-file',
-  mandataire: '',
-  chefDeFile: '',
-}
+  authorizationStrategy: "chef-de-file",
+  mandataire: "",
+  chefDeFile: "",
+};
 
 const ClientForm = () => {
-  const router = useRouter()
-  const {clientId, demo} = router.query
-  const isDemo = demo === '1'
-  const [isFormValid, setIsFormValid] = useState(false)
-  const [submitError, setSubmitError] = useState()
-  const [isLoading, setIsLoading] = useState(true)
-  const [isformChefDeFileOpen, setIsformChefDeFileOpen] = useState<boolean>(false)
-  const [initialChefDeFileForm, setInitialChefDeFileForm] = useState<ChefDeFileApiDepotType>(null)
-  const [chefsDeFileOptions, setChefsDeFileOptions] = useState<ChefDeFileApiDepotType[]>(null)
-  const [mandatairesOptions, setMandatairesOptions] = useState<MandataireApiDepotType[]>(null)
-  const [formData, setFormData] = useState(null)
+  const router = useRouter();
+  const { clientId, demo } = router.query;
+  const isDemo = demo === "1";
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [submitError, setSubmitError] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isformChefDeFileOpen, setIsformChefDeFileOpen] =
+    useState<boolean>(false);
+  const [initialChefDeFileForm, setInitialChefDeFileForm] =
+    useState<ChefDeFileApiDepotType>(null);
+  const [chefsDeFileOptions, setChefsDeFileOptions] =
+    useState<ChefDeFileApiDepotType[]>(null);
+  const [mandatairesOptions, setMandatairesOptions] =
+    useState<MandataireApiDepotType[]>(null);
+  const [formData, setFormData] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      setIsLoading(true)
-      const mandataires: MandataireApiDepotType[] = await getMandataires(isDemo)
-      setMandatairesOptions(mandataires)
-      const chefsDeFile: ChefDeFileApiDepotType[] = await getChefsDeFile(isDemo)
-      setChefsDeFileOptions(chefsDeFile)
+      setIsLoading(true);
+      const mandataires: MandataireApiDepotType[] =
+        await getMandataires(isDemo);
+      setMandatairesOptions(mandataires);
+      const chefsDeFile: ChefDeFileApiDepotType[] =
+        await getChefsDeFile(isDemo);
+      setChefsDeFileOptions(chefsDeFile);
       if (clientId) {
-        const client = await getClient(clientId, isDemo)
+        const client = await getClient(clientId, isDemo);
         setFormData({
           nom: client.nom,
           isModeRelax: Boolean(client.options?.relaxMode),
@@ -59,184 +76,205 @@ const ClientForm = () => {
           authorizationStrategy: client.authorizationStrategy,
           mandataire: client.mandataire,
           chefDeFile: client.chefDeFile,
-        })
+        });
       } else {
-        setFormData(newClientFormData)
+        setFormData(newClientFormData);
       }
 
-
-      setIsLoading(false)
+      setIsLoading(false);
     }
 
-    void fetchData()
-  }, [clientId, isDemo])
+    void fetchData();
+  }, [clientId, isDemo]);
 
-  const handleEdit = useCallback((property: string) => (value: any) => {
-    setFormData(state => ({...state, [property]: value}))
-  }, [])
+  const handleEdit = useCallback(
+    (property: string) => (value: any) => {
+      setFormData((state) => ({ ...state, [property]: value }));
+    },
+    []
+  );
 
-  const handleToggle = useCallback((property: string) => () => {
-    setFormData(state => ({...state, [property]: !state[property]}))
-  }, [])
+  const handleToggle = useCallback(
+    (property: string) => () => {
+      setFormData((state) => ({ ...state, [property]: !state[property] }));
+    },
+    []
+  );
 
-  const handleSumit = async event => {
-    event.preventDefault()
-    const {nom, isModeRelax, isActive, authorizationStrategy, mandataire, chefDeFile} = formData
+  const handleSumit = async (event) => {
+    event.preventDefault();
+    const {
+      nom,
+      isModeRelax,
+      isActive,
+      authorizationStrategy,
+      mandataire,
+      chefDeFile,
+    } = formData;
     const body = {
       nom,
-      options: {relaxMode: isModeRelax},
+      options: { relaxMode: isModeRelax },
       active: isActive,
       authorizationStrategy,
       mandataire: undefined,
       chefDeFile,
-    }
+    };
 
     try {
       // Gestion du mandataire sélectionné ou créé
-      if (typeof mandataire === 'object') {
-        const newMandataire = await createMandataire(mandataire, isDemo)
-        body.mandataire = newMandataire._id
+      if (typeof mandataire === "object") {
+        const newMandataire = await createMandataire(mandataire, isDemo);
+        body.mandataire = newMandataire._id;
       } else {
-        body.mandataire = mandataire
+        body.mandataire = mandataire;
       }
 
-      let _clientId = clientId
+      let _clientId = clientId;
       if (_clientId) {
-        await updateClient(_clientId, body, isDemo)
+        await updateClient(_clientId, body, isDemo);
       } else {
-        const response = await createClient(body, isDemo)
-        _clientId = response._id
+        const response = await createClient(body, isDemo);
+        _clientId = response._id;
       }
 
-      await router.push({pathname: '/api-depot/client', query: {clientId: _clientId, demo: isDemo ? 1 : 0}})
+      await router.push({
+        pathname: "/api-depot/client",
+        query: { clientId: _clientId, demo: isDemo ? 1 : 0 },
+      });
     } catch (error: unknown) {
-      setSubmitError((error as any).message || '')
+      setSubmitError((error as any).message || "");
     }
-  }
+  };
 
   useEffect(() => {
     if (!formData) {
-      return
+      return;
     }
 
-    const {nom, authorizationStrategy, mandataire, chefDeFile} = formData
+    const { nom, authorizationStrategy, mandataire, chefDeFile } = formData;
 
-    let isFormValid = nom && mandataire
+    let isFormValid = nom && mandataire;
 
-    if (authorizationStrategy === 'chef-de-file' && !chefDeFile) {
-      isFormValid = false
+    if (authorizationStrategy === "chef-de-file" && !chefDeFile) {
+      isFormValid = false;
     }
 
-    setIsFormValid(isFormValid)
-  }, [formData])
+    setIsFormValid(isFormValid);
+  }, [formData]);
 
   const closeFormChefDeFile = async () => {
-    const chefsDeFile: ChefDeFileApiDepotType[] = await getChefsDeFile(isDemo)
-    setChefsDeFileOptions(chefsDeFile)
-    setInitialChefDeFileForm(null)
-    setIsformChefDeFileOpen(false)
-  }
-
+    const chefsDeFile: ChefDeFileApiDepotType[] = await getChefsDeFile(isDemo);
+    setChefsDeFileOptions(chefsDeFile);
+    setInitialChefDeFileForm(null);
+    setIsformChefDeFileOpen(false);
+  };
 
   const openFormChefDeFile = (initialId: string = null) => {
-    setInitialChefDeFileForm(chefsDeFileOptions.find(c => c._id == initialId))
-    setIsformChefDeFileOpen(true)
-  }
+    setInitialChefDeFileForm(
+      chefsDeFileOptions.find((c) => c._id == initialId)
+    );
+    setIsformChefDeFileOpen(true);
+  };
 
   return (
     <Loader isLoading={isLoading}>
-      {formData && <div className='fr-container fr-my-4w'>
-        <form onSubmit={handleSumit}>
-          <TextInput
-            label='Nom'
-            value={formData.nom}
-            hint='Nom du client. Exemple : Business Geografic'
-            onChange={e => {
-              handleEdit('nom')(e.target.value)
-            }}
-          />
+      {formData && (
+        <div className="fr-container fr-my-4w">
+          <form onSubmit={handleSumit}>
+            <TextInput
+              label="Nom"
+              value={formData.nom}
+              hint="Nom du client. Exemple : Business Geografic"
+              onChange={(e) => {
+                handleEdit("nom")(e.target.value);
+              }}
+            />
 
-          <ToggleSwitch
-            label='Activé'
-            helperText='Authorise le client à utiliser l’API'
-            checked={formData.isActive}
-            onChange={handleToggle('isActive')}
-          />
+            <ToggleSwitch
+              label="Activé"
+              helperText="Authorise le client à utiliser l’API"
+              checked={formData.isActive}
+              onChange={handleToggle("isActive")}
+            />
 
-          <ToggleSwitch
-            label='Mode relax'
-            helperText='Le mode relax assoupli les vérifications du Validateur BAL'
-            checked={formData.isModeRelax}
-            onChange={handleToggle('isModeRelax')}
-          />
+            <ToggleSwitch
+              label="Mode relax"
+              helperText="Le mode relax assoupli les vérifications du Validateur BAL"
+              checked={formData.isModeRelax}
+              onChange={handleToggle("isModeRelax")}
+            />
 
-          <SelectInput
-            label='Stratégie d’authorisation'
-            hint='Méthode qui authorise le client à publier des BAL'
-            value={formData.authorizationStrategy}
-            options={authorizationStrategyOptions}
-            handleChange={handleEdit('authorizationStrategy')}
-          />
+            <SelectInput
+              label="Stratégie d’authorisation"
+              hint="Méthode qui authorise le client à publier des BAL"
+              value={formData.authorizationStrategy}
+              options={authorizationStrategyOptions}
+              handleChange={handleEdit("authorizationStrategy")}
+            />
 
-          <MandataireForm
-            selectedMandataire={formData.mandataire}
-            mandataires={mandatairesOptions}
-            onSelect={handleEdit('mandataire')}
-          />
+            <MandataireForm
+              selectedMandataire={formData.mandataire}
+              mandataires={mandatairesOptions}
+              onSelect={handleEdit("mandataire")}
+            />
 
-          {formData.authorizationStrategy === 'chef-de-file' && (
+            {formData.authorizationStrategy === "chef-de-file" && (
+              <>
+                {isformChefDeFileOpen ? (
+                  <ChefDeFileForm
+                    initialChefDeFile={initialChefDeFileForm}
+                    onSelect={handleEdit("chefDeFile")}
+                    isDemo={isDemo}
+                    close={() => closeFormChefDeFile()}
+                  />
+                ) : (
+                  <>
+                    <ChefDeFileSelect
+                      selectedChefDeFile={formData.chefDeFile}
+                      chefsDeFile={chefsDeFileOptions}
+                      onSelect={handleEdit("chefDeFile")}
+                    />
+                    <div className="fr-my-4w">
+                      <div className="fr-grid-row">
+                        <Button onClick={(e) => openFormChefDeFile()}>
+                          Créer
+                        </Button>
+                        <Button
+                          priority="secondary"
+                          onClick={(e) =>
+                            openFormChefDeFile(formData.chefDeFile)
+                          }
+                        >
+                          Modifier
+                        </Button>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </>
+            )}
 
-            <>
-              { isformChefDeFileOpen ? 
-                <ChefDeFileForm
-                  initialChefDeFile={initialChefDeFileForm}
-                  isDemo={isDemo}
-                  close={() => closeFormChefDeFile()}
-                  /> 
-                :
-                <>
-                <ChefDeFileSelect
-                  selectedChefDeFile={formData.chefDeFile}
-                  chefsDeFile={chefsDeFileOptions}
-                  onSelect={handleEdit('chefDeFile')}
-                />
-                <div className='fr-my-4w'>
-                  <div className='fr-grid-row'>
-                    <Button onClick={e => openFormChefDeFile()}>
-                      Créer
-                    </Button>
-                    <Button priority='secondary' onClick={e => openFormChefDeFile(formData.chefDeFile)}>
-                      Modifier
-                    </Button>
-                  </div>
-                </div>
+            <Button
+              type="submit"
+              iconId="fr-icon-save-line"
+              disabled={!isFormValid}
+            >
+              Enregistrer
+            </Button>
+          </form>
 
-                </>
-                }
-            </>
-
+          {submitError && (
+            <Alert
+              className="fr-mt-2w"
+              severity="error"
+              title="Impossible d’enregistrer"
+              description={submitError}
+            />
           )}
-
-          <Button
-            type='submit'
-            iconId='fr-icon-save-line'
-            disabled={!isFormValid}
-          >
-            Enregistrer
-          </Button>
-        </form>
-
-        {submitError && (
-          <Alert
-            className='fr-mt-2w'
-            severity='error'
-            title='Impossible d’enregistrer'
-            description={submitError}
-          />
-        )}
-      </div>}
+        </div>
+      )}
     </Loader>
-  )
-}
+  );
+};
 
-export default ClientForm
+export default ClientForm;
