ecfa0c00201296f6bcc79a90d6b8128fb809e5e8
Fufeck <fabien.tafforeau@gmail.com>
2024-08-26 17:13:06 +0200

    migration: postgres mes-adresses

--- types/mes-adresses.ts
+++ types/mes-adresses.ts
@@ -1,19 +1,35 @@
+export enum StatusBaseLocalEnum {
+  DRAFT = "draft",
+  PUBLISHED = "published",
+  DEMO = "demo",
+  REPLACED = "replaced",
+}
+
+export enum StatusSyncEnum {
+  OUTDATED = "outdated",
+  SYNCED = "synced",
+  CONFLICT = "conflict",
+}
 
 export type BaseLocaleType = {
-  _id: string;
+  id: string;
   token?: string;
   nom?: string;
-  status?: string;
+  status?: StatusBaseLocalEnum;
   commune?: string;
   nbNumeros?: number;
   nbNumerosCertifies?: number;
   isAllCertified?: boolean;
   commentedNumeros?: any;
-  enableComplement?: boolean;
   emails?: string[];
-  sync?: any;
-  _created?: string;
-  _updated?: string;
-  _deleted?: string;
+  sync?: {
+    status: StatusSyncEnum;
+    isPaused: boolean;
+    currentUpdated: string;
+    lastUploadedRevisionId: string;
+  };
+  createdAt?: string;
+  updatedAt?: string;
+  deletedAt?: string;
   habilitationIsValid?: boolean;
-}
+};
