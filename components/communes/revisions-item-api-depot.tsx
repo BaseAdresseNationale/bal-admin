2bd7584a31c480d14c9ce1112d731b7bf6285235
Fufeck <fabien.tafforeau@gmail.com>
2025-01-08 10:35:33 +0100

    feat: add download file bal to revisions list in commune page

--- components/communes/revisions-item-api-depot.tsx
+++ components/communes/revisions-item-api-depot.tsx
@@ -1,45 +1,79 @@
-import Link from 'next/link'
-import {Badge} from '@codegouvfr/react-dsfr/Badge'
+import Link from "next/link";
+import { Badge } from "@codegouvfr/react-dsfr/Badge";
 
-import type {RevisionApiDepotType} from '../../types/api-depot'
-import {formatDate} from '@/lib/util/date'
-import MongoId from '@/components/mongo-id'
-import Tooltip from '@/components/tooltip'
+import type { RevisionApiDepotType } from "../../types/api-depot";
+import { formatDate } from "@/lib/util/date";
+import MongoId from "@/components/mongo-id";
+import Tooltip from "@/components/tooltip";
 
-export const RevisionItemApiDepot = (
-  {_id, status, current = false, client, validation, createdAt, publishedAt = ''}: RevisionApiDepotType,
-) => (
+export const RevisionItemApiDepot = ({
+  _id,
+  status,
+  current = false,
+  client,
+  validation,
+  createdAt,
+  publishedAt = "",
+}: RevisionApiDepotType) => (
   <tr key={_id}>
-    <td className='fr-col fr-my-1v'>
+    <td className="fr-col fr-my-1v">
       <MongoId id={_id} />
     </td>
-    <td className='fr-col fr-my-1v'>
+    <td className="fr-col fr-my-1v">
       <Tooltip text={client.nom}>
-        <Link legacyBehavior passHref href={{pathname: '/api-depot/client', query: {clientId: client._id}}}>
+        <Link
+          legacyBehavior
+          passHref
+          href={{
+            pathname: "/api-depot/client",
+            query: { clientId: client._id },
+          }}
+        >
           {client._id}
         </Link>
       </Tooltip>
     </td>
-    <td className='fr-col fr-my-1v'>
-      <Badge severity={status === 'published' ? 'success' : 'warning'} noIcon>{status}</Badge>
+    <td className="fr-col fr-my-1v">
+      <Badge severity={status === "published" ? "success" : "warning"} noIcon>
+        {status}
+      </Badge>
     </td>
-    <td className='fr-col fr-my-1v'>
-      <input type='checkbox' id='checkbox' name='checkbox' checked={current} disabled />
+    <td className="fr-col fr-my-1v">
+      <input
+        type="checkbox"
+        id="checkbox"
+        name="checkbox"
+        checked={current}
+        disabled
+      />
     </td>
-    <td className='fr-col fr-my-1v'>
+    <td className="fr-col fr-my-1v">
       {validation.valid ? (
-        <input type='checkbox' id='checkbox' name='checkbox' checked disabled />
+        <input type="checkbox" id="checkbox" name="checkbox" checked disabled />
       ) : (
-        <Tooltip text={validation?.errors?.join(',') || 'Erreur inconnue'}>
-          <input type='checkbox' id='checkbox' name='checkbox' disabled />
+        <Tooltip text={validation?.errors?.join(",") || "Erreur inconnue"}>
+          <input type="checkbox" id="checkbox" name="checkbox" disabled />
         </Tooltip>
       )}
     </td>
-    <td className='fr-col fr-my-1v'>
-      {createdAt ? formatDate(createdAt) : 'inconnu'}
+    <td className="fr-col fr-my-1v">
+      {createdAt ? formatDate(createdAt) : "inconnu"}
     </td>
-    <td className='fr-col fr-my-1v'>
-      {publishedAt ? formatDate(publishedAt) : 'inconnu'}
+    <td className="fr-col fr-my-1v">
+      {publishedAt ? formatDate(publishedAt) : "inconnu"}
+    </td>
+    <td className="fr-col fr-my-1v">
+      {status == "published" && (
+        <Link
+          target="_blank"
+          passHref
+          href={{
+            pathname: `${process.env.NEXT_PUBLIC_API_DEPOT_URL}/revisions/${_id}/files/bal/download`,
+          }}
+        >
+          Télécharger
+        </Link>
+      )}
     </td>
   </tr>
-)
+);
