5936fcd43d56b3852fe64352b4b4f51360a3f2c6
Fufeck <fabien.tafforeau@gmail.com>
2025-04-15 15:46:04 +0200

    fix: cron send mail the day before event

--- server/lib/events/cron.ts
+++ server/lib/events/cron.ts
@@ -5,15 +5,34 @@ import { Participant } from "../participant/entity";
 import { findManyByEvent } from "../participant/service";
 import { sendParticipationEvenement } from "../mailer/service";
 import { Logger } from "../../utils/logger.utils";
+import { addDays } from "date-fns";
+
+const adminParticipants: Participant[] = [
+  {
+    eventId: null,
+    fullname: "Olivier Bourreau",
+    community: "BAL",
+    function: "ADMIN",
+    email: "olivier.bourreau@beta.gouv.fr",
+  },
+  {
+    eventId: null,
+    fullname: "Yoann Merrien",
+    community: "BAL",
+    function: "ADMIN",
+    email: "yoann.merrien@beta.gouv.fr",
+  },
+];
 
 async function sendReminderEvents() {
-  const today = new Date().toISOString().split("T")[0];
+  const today = addDays(new Date(), 1).toISOString().split("T")[0];
   const events: Event[] = await EventService.findMany({
     date: today,
     reminderSent: false,
   });
   for (const event of events) {
     const participants: Participant[] = await findManyByEvent(event.id);
+    participants.push(...adminParticipants);
     for (const participant of participants) {
       try {
         await sendParticipationEvenement(event, participant);
