7b70390692b448e9eb5c8613f18d81e7a719eb61
Guillaume Fay <guillaume_fay@hotmail.com>
2025-05-09 17:32:13 +0200

    fix: url

--- lib/api-signalement.ts
+++ lib/api-signalement.ts
@@ -1,12 +1,12 @@
 import { SignalementStatusEnum } from "types/signalement.types";
 
-const API_SIGNALMENT_URL = process.env.NEXT_PUBLIC_API_SIGNALEMENT_URL;
-
 export const getSignalementCount = async (
   code: string,
   status: SignalementStatusEnum
 ): Promise<number> => {
-  const url = new URL(`${API_SIGNALMENT_URL}/signalements`);
+  const url = new URL(
+    `${process.env.NEXT_PUBLIC_API_SIGNALEMENT_URL}/signalements`
+  );
   url.searchParams.append("codeCommunes", code);
   url.searchParams.append("status", status);
   url.searchParams.append("limit", "1");
