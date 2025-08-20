82f1f8f8832b8fd01cf94f7d7ee4c80fe779fa65
Guillaume Fay <guillaume_fay@hotmail.com>
2025-08-20 14:13:26 +0200

    fix: remove send mail from bal-widget (#116)

--- server/lib/bal-widget/controller.ts
+++ server/lib/bal-widget/controller.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import routeGuard from "../../route-guard";
 import * as BALWidgetService from "./service";
-import * as MailerService from "../mailer/service";
 import { Logger } from "../../utils/logger.utils";
 
 const BALWidgetRoutes = express.Router();
@@ -26,26 +25,6 @@ BALWidgetRoutes.post("/config", routeGuard, async (req, res) => {
     res.json(config);
   } catch (err) {
     Logger.error(`Impossible de créer le config de BalWidget`, err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-BALWidgetRoutes.post("/send-mail", async (req, res) => {
-  try {
-    await MailerService.sendFormContactMail(req.body);
-    res.json(true);
-  } catch (err) {
-    Logger.error(`Impossible d'envoyer l'email de contact`, err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-BALWidgetRoutes.post("/send-mail-to-commune", async (req, res) => {
-  try {
-    await MailerService.sendSignalementToCommune(req.body);
-    res.json(true);
-  } catch (err) {
-    Logger.error(`Impossible d'envoyer l'email à la commune`, err);
     res.status(500).json({ error: err.message });
   }
 });
