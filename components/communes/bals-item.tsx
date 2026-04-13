d9be0558d6365904d02118c42cb767f3b4d1c155
Fufeck <fabien.tafforeau@gmail.com>
2026-04-13 15:28:40 +0200

    feat: add import type bals

--- components/communes/bals-item.tsx
+++ components/communes/bals-item.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 import { Badge } from "@codegouvfr/react-dsfr/Badge";
 import { Button } from "@codegouvfr/react-dsfr/Button";
 
-import type { BaseLocaleType } from "../../types/mes-adresses";
+import { BaseLocaleType, ImportTypeEnum } from "../../types/mes-adresses";
 import { computeStatus } from "@/lib/bal-status";
 import { formatDate } from "@/lib/util/date";
 
 export const BalsItem = (
   item: BaseLocaleType,
   actions: Record<string, (item: BaseLocaleType) => void>,
-  selectedItem?: string
+  selectedItem?: string,
 ) => {
   const {
     id,
@@ -21,9 +21,20 @@ export const BalsItem = (
     updatedAt,
     nbNumeros,
     nbNumerosCertifies,
+    importType,
   } = item;
 
   const computedStatus = computeStatus(status, sync);
+  const getSeverityImport = () => {
+    if (importType === ImportTypeEnum.API_DEPOT) {
+      return "success";
+    } else if (importType === ImportTypeEnum.BAN) {
+      return "info";
+    } else if (importType === ImportTypeEnum.CSV) {
+      return "new";
+    }
+    return "warning";
+  };
 
   return (
     <tr key={id}>
@@ -35,16 +46,21 @@ export const BalsItem = (
         </Badge>
       </td>
       <td className="fr-col fr-my-1v">
-        {createdAt ? formatDate(createdAt) : "inconnu"}
+        {createdAt ? formatDate(createdAt, "Pp") : "inconnu"}
       </td>
       <td className="fr-col fr-my-1v">
-        {updatedAt ? formatDate(updatedAt) : "inconnu"}
+        {updatedAt ? formatDate(updatedAt, "Pp") : "inconnu"}
       </td>
       <td className="fr-col fr-my-1v">
         {emails ? emails.join("\n") : "inconnu"}
       </td>
       <td className="fr-col fr-my-1v">
         {nbNumeros} / {nbNumerosCertifies}
+      </td>
+      <td className="fr-col fr-my-1v">
+        <Badge severity={getSeverityImport()} noIcon>
+          {importType || "Inconnu"}
+        </Badge>
       </td>
       <td className="fr-col fr-my-1v">
         <div style={{ width: "150px" }}>
